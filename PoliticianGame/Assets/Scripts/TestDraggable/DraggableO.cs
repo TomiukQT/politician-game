ac731db31958bd8fe5dee08e538c925bfabc5dcb
TomiukQT <TomiukQT@gmail.com>
2019-09-05 23:42:34 +0200

    DropZone as GO

--- PoliticianGame/Assets/Scripts/TestDraggable/DraggableO.cs
+++ PoliticianGame/Assets/Scripts/TestDraggable/DraggableO.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class DraggableO : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
- 
+
+    private Transform m_ParentToReturn;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("Begin Dragging");
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -18,14 +20,28 @@ public class DraggableO : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
         Debug.Log("Dragging");
         transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
                                           Camera.main.ScreenToWorldPoint(Input.mousePosition).y,
-                                          0.0f);
+                                          -0.5f);
 
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        
+        CheckForDropZones();
+        this.transform.SetParent(m_ParentToReturn);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
     }
 
+    void CheckForDropZones()
+    {
+        RaycastHit[] hits;
+        Ray ray = GameObject.Find("Main Camera").GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        hits = Physics.RaycastAll(ray, 100f, LayerMask.GetMask("DropZone"));
+        if (hits.Length > 0)
+        {
+            Debug.Log(hits[0].transform.gameObject.name);
+            m_ParentToReturn = hits[0].transform;
+        }
+    }
 
 }
--- PoliticianGame/Assets/Scripts/TestDraggable/DropZoneO.cs
+++ PoliticianGame/Assets/Scripts/TestDraggable/DropZoneO.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.EventSystems; 
+using UnityEngine.EventSystems;
 using UnityEngine;
 
-public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+public class DropZoneO : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
     private DeckManager deckManager;
 
@@ -15,40 +15,64 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        Debug.Log("Entered: " + gameObject.name);
         if (eventData.pointerDrag == null)
             return;
+        
+        /*
         Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
 
         if (d != null)
         {
             d.m_PlaceholderParent = this.transform;
         }
-          Debug.Log("Entered: " + gameObject.name);
-
+        */
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        Debug.Log("Exit: " + gameObject.name);
         if (eventData.pointerDrag == null)
             return;
-          Debug.Log("Exit: " + gameObject.name);
+          
+        /*
         Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
 
         if (d != null && d.m_PlaceholderParent == this.transform)
         {
             d.m_PlaceholderParent = d.m_ParentToReturn;
         }
+        */
     }
 
     public void OnDrop(PointerEventData eventData)
     {
+        /*
         Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
 
         if (d != null)
         {
-            if(deckManager.SpendResource(eventData.pointerDrag.GetComponent<CardManager>().m_Card.cost))
+            if (deckManager.SpendResource(eventData.pointerDrag.GetComponent<CardManager>().m_Card.cost))
                 d.m_ParentToReturn = this.transform;
         }
+        */
         Debug.Log("drasDropTO: " + gameObject.name);
+    }
+
+    void Update()
+    {
+        CheckForDropZones();
+    }
+
+    void CheckForDropZones()
+    {
+        RaycastHit[] hits;
+        Ray ray = GameObject.Find("Main Camera").GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        hits = Physics.RaycastAll(ray, 100f, LayerMask.GetMask("DropZone"));
+        if (hits.Length > 0)
+        {
+            Debug.Log(hits[0].transform.gameObject.name);
+
+        }
     }
 }
